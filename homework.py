10b465e1c0d9d8eb7740603f18e4efd99c063439
Sweety_Bug <maxim_terekhin@bk.ru>
2022-06-10 19:40:08 +0300

    Преобразование в ф-ии

--- homework.py
+++ homework.py
@@ -3,50 +3,62 @@
 #Дополнительно: Добавить возможность использования скобок, меняющих приоритет операций. 
 #Пример: 1+2*3 => 7; (1+2)*3 => 9;
 
+def find(nums):
+    if '^' in nums:
+        abs = nums.index("^")
+        nums[abs] = int(nums[abs-1]) ** int(nums[abs+1])
+        del nums[abs+1]
+        del nums[abs-1]
+        find(nums)
+    return nums
+
+def create_array(sp, stroka):
+    arr = [i for i in stroka]
+    nums = []
+    s = ''
+    for i in arr:
+        if i in sp:
+            s += i
+        else:
+            nums.append(s)
+            nums.append(i)
+            s = ''
+    nums.append(s)
+    return nums
+
+def znach(nums):
+    i = 0
+    while i < len(nums):
+        if nums[i] == '*':
+            nums[i] = int(nums[i-1]) * int(nums[i+1])
+            del nums[i+1]
+            del nums[i-1]
+            i -= 1
+        elif nums[i] == '/':
+            nums[i] = int(nums[i-1]) / int(nums[i+1])
+            del nums[i+1]
+            del nums[i-1]
+            i -= 1
+        i += 1
+    print(nums)
+    znach = int(nums[0])
+    for i in range(len(nums)):
+        if nums[i] == '+':
+            znach += int(nums[i+1])
+        elif nums[i] == '-':
+            znach -= int(nums[i+1])
+    print(znach)
+
 n = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-exp = '2+5^2-15/3'
-arr = [i for i in exp]
-nums = []
-s = ''
-for i in arr:
-    if i in n:
-        s += i
-    else:
-        nums.append(s)
-        nums.append(i)
-        s = ''
-nums.append(s)
-i = 0
-a = True
-count = nums.count('^')
-print(nums)
-g = 0
-while g < count:
-    abs = nums.index("^")
-    nums[abs] = int(nums[abs-1]) ** int(nums[abs+1])
-    del nums[abs+1]
-    del nums[abs-1]
-    g += 1
-while i < len(nums):
-    if nums[i] == '*':
-        nums[i] = int(nums[i-1]) * int(nums[i+1])
-        del nums[i+1]
-        del nums[i-1]
-        i -= 1
-    elif nums[i] == '/':
-        nums[i] = int(nums[i-1]) / int(nums[i+1])
-        del nums[i+1]
-        del nums[i-1]
-        i -= 1
-    i += 1
-print(nums)
-znach = int(nums[0])
-for i in range(len(nums)):
-    if nums[i] == '+':
-        znach += int(nums[i+1])
-    elif nums[i] == '-':
-        znach -= int(nums[i+1])
-print(znach)
+exp = '1+2*3'
+
+array = create_array(n, exp)
+print(array)
+array = find(array)
+print(array)
+znach(array)
+
+
 
 
 #2 - Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных. 
